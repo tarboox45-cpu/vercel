a76c94fd5185b5c52db196dfea357f6bdf7b98f7
tarboox45-cpu <tarboox45@gmail.com>
2025-12-18 10:51:00 +0200

    Add files via upload

--- api/download.js
+++ api/download.js
@@ -125,19 +125,19 @@ MONITORING_ENABLED=false
 # COLOR & STYLING LIBRARY
 # ============================================
 # Primary Colors
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[1;33m'
-BLUE='\033[0;34m'
-CYAN='\033[0;36m'
-MAGENTA='\033[0;35m'
-WHITE='\033[1;37m'
-NC='\033[0m'
+RED=' \x1b[0;31m'
+GREEN=' \x1b[0;32m'
+YELLOW=' \x1b[1;33m'
+BLUE=' \x1b[0;34m'
+CYAN=' \x1b[0;36m'
+MAGENTA=' \x1b[0;35m'
+WHITE=' \x1b[1;37m'
+NC=' \x1b[0m'
 
 # Styles
-BOLD='\033[1m'
-UNDERLINE='\033[4m'
-BLINK='\033[5m'
+BOLD=' \x1b[1m'
+UNDERLINE=' \x1b[4m'
+BLINK=' \x1b[5m'
 
 # Custom Panel Colors
 HEADER_COLOR="${CYAN}${BOLD}"
